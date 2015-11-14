0d1ee4ae82dcc1ff2a4deae0b652804748fd2d77
mcomas <marc@macmini.local>
2015-11-14 14:11:46 +0100

    First commit

---
+++ comparing.R
@@ -0,0 +1,20 @@
+library(mclust)
+
+data(Baudry_etal_2010_JCGS_examples)
+mc = clustCombi(ex4.2)
+plot(mc, ex4.2)
+post = mc$MclustOutput$z
+
+library(mixpack)
+L8 = list(diag(NCOL(post)),post) 
+L7 = mergeStep_const_entropy(L8[[2]])
+L6 = mergeStep_const_entropy(L7[[2]])
+L5 = mergeStep_const_entropy(L6[[2]])
+L4 = mergeStep_const_entropy(L5[[2]])
+L3 = mergeStep_const_entropy(L4[[2]])
+L2 = mergeStep_const_entropy(L3[[2]])
+
+##Hi han diferencies
+
+L8[[1]] %*% L7[[1]]%*% L6[[1]] %*% L5[[1]] %*% L4[[1]] %*% L3[[1]] %*% L2[[1]]
+table(mc$classification[[8]], mc$classification[[2]])
