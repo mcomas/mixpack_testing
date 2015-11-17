5893c8a0077b57802efb5d9e995d0fce79bbc16e
marc_comas <marc.comas@gmail.com>
2015-11-17 16:54:55 +0100

    Altres fonts d'instal·lació

--- comparing.R
+++ comparing.R
@@ -17,3 +17,31 @@ L2 = mergeStep(L3[[2]], 'const', 'entropy')
 
 L8[[1]] %*% L7[[1]]%*% L6[[1]] %*% L5[[1]] %*% L4[[1]] %*% L3[[1]] %*% L2[[1]]
 table(mc$classification[[8]], mc$classification[[2]])
+
+
+xlog = function(x) ifelse(x==0, 0, x * log(x))
+l_lambda = list(
+  'entr' = function(v_tau, a, b) xlog(v_tau[a] + v_tau[b]) - xlog(v_tau[a]) - xlog(v_tau[b]),
+  'demp' = function(v_tau, a, b) if(which.max(v_tau) == b) 1 else 0,
+  'demp.mod' = function(v_tau, a, b) v_tau[b] * (v_tau[a] + v_tau[b])^-1,
+  'coda' = function(v_tau, a, b) log(v_tau[b] / v_tau[a]),
+  'coda.norm' = function(v_tau, a, b) -log(v_tau[b] / v_tau[a])^2,
+  'prop' = function(v_tau, a, b) v_tau[b] )
+
+# Weitghing functions
+l_omega = list(
+  'cnst' = function(v_tau, a) 1,
+  'prop' = function(v_tau, a) v_tau[a],
+  'dich' = function(v_tau, a) if(which.max(v_tau) == a) 1 else 0
+)
+
+fun1 = function(post){
+  mixpack::get_hierarchical_partition(post, l_omega[['cnst']], l_lambda[['entr']])
+}
+fun2 = function(post){
+  mixpack::get_hierarchical_partition_fast(post, 'const', 'entropy')
+}
+
+
+system.time(hp1 <- fun1(post))
+system.time(hp2 <- fun2(post))
--- install_mixpack.R
+++ install_mixpack.R
@@ -1,4 +1,5 @@
-library(devtools)
+
 devtools::install_github('mcomas/mixpack')
 
-
+devtools::install_git('/home/marc/research/packages/mixpack/')
+devtools::install_local('/home/marc/research/packages/mixpack/')
